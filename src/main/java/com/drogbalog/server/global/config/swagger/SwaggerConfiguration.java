925eda1cdbae981c0d817e608f6906478bd28e2e
limwoobin <drogba02@naver.com>
2020-12-06 14:55:44 +0900

    Drogbalog Server
    - 프로젝트 구조 수정

--- src/main/java/com/drogbalog/server/global/config/swagger/SwaggerConfiguration.java
+++ src/main/java/com/drogbalog/server/global/config/swagger/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package com.drogbalog.server.config;
+package com.drogbalog.server.global.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
