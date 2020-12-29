055a8acf6034eb7c29b9e22f9f680f5cd45219d0
limwoobin <drogba02@naver.com>
2020-12-29 18:06:13 +0900

    Delete - Comment Directory

--- src/main/java/com/drogbalog/server/comment/domain/dto/CommentDto.java
+++
@@ -1,9 +0,0 @@
-package com.drogbalog.server.comment.domain.dto;
-
-import io.swagger.annotations.ApiModel;
-import lombok.Data;
-
-@Data
-@ApiModel(value = "Comment 데이터 모델")
-public class CommentDto {
-}
--- src/main/java/com/drogbalog/server/comment/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.drogbalog.server.comment.repository;
-
-import com.drogbalog.server.comment.domain.entity.CommentEntity;
-import com.drogbalog.server.global.config.jpa.DefaultRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends DefaultRepository<CommentEntity , Long> {
-}
