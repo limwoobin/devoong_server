06e9b617ba3d6fe7eae896b66d02a085f704a18c
limwoobin <drogba02@naver.com>
2020-12-31 14:08:46 +0900

    Add - Oauth2 설정 # 3
    LoginUser Annotation 추가

--- src/main/java/com/drogbalog/server/global/config/web/WebConfig.java
+++ src/main/java/com/drogbalog/server/global/config/web/WebConfig.java
@@ -1,13 +1,20 @@
 package com.drogbalog.server.global.config.web;
 
+import com.drogbalog.server.global.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
+import java.util.List;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
     private static final long MAX_SECONDS = 3600;
 
     @Override
@@ -20,4 +27,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(MAX_SECONDS);
     }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
 }
