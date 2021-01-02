deed354ecf190d36c95e336cab2619e951df9c99
limwoobin <drogba02@naver.com>
2021-01-02 14:44:42 +0900

    Fix - Validator List 로 변경

--- src/main/java/com/drogbalog/server/user/service/UserHistoryService.java
+++ src/main/java/com/drogbalog/server/user/service/UserHistoryService.java
@@ -1,17 +1,10 @@
 package com.drogbalog.server.user.service;
 
-import com.drogbalog.server.user.dao.UserHistoryDao;
-import com.drogbalog.server.user.domain.dto.UserHistoryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserHistoryService {
-    private final UserHistoryDao userHistoryDao;
 
-    public UserHistoryDto getUserLoginHistory(long userId) {
-        UserHistoryDto userHistoryDto = userHistoryDao.getUserLoginHistory(userId);
-        return userHistoryDto;
-    }
 }
--- src/main/java/com/drogbalog/server/user/validator/UserValidator.java
+++ src/main/java/com/drogbalog/server/user/validator/UserValidator.java
@@ -7,18 +7,22 @@ import com.drogbalog.server.user.validator.impl.NickNameValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Component
 public class UserValidator {
     private final UserDao userDao;
 
     public boolean signUpValidationCheck(UserRequest request) {
-        Validator emailValidator = new EmailValidator(userDao);
-        boolean isEmail = emailValidator.signUpValidator(request);
+        List<Validator> validators = Arrays.asList(new EmailValidator(userDao) , new NickNameValidator(userDao));
+        boolean isUserRequest = false;
 
-        Validator nickNameValidator = new NickNameValidator(userDao);
-        boolean isNickName = nickNameValidator.signUpValidator(request);
+        for (Validator validator : validators) {
+            isUserRequest = validator.signUpValidator(request);
+        }
 
-        return isEmail && isNickName;
+        return isUserRequest;
     }
 }
