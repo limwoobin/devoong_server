3ccf517ce4dd9e25077c32d6778740d770928ab7
limwoobin <drogba02@naver.com>
2021-01-04 14:18:24 +0900

    Fix - .gitignore 수정

--- src/main/java/com/drogbalog/server/user/service/UserService.java
+++ src/main/java/com/drogbalog/server/user/service/UserService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService  {
+public class UserService {
     private final UserDao userDao;
     private final PasswordEncoder passwordEncoder;
     private final UserValidator userValidator;
