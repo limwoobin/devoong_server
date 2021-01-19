72792c74fa3a255feeba91544b3aa15dee47aaed
limwoobin <drogba02@naver.com>
2021-01-19 18:18:44 +0900

    Fix - Validator 메서드 이름 수정

--- src/main/java/com/drogbalog/server/domain/user/service/validator/Validator.java
+++ src/main/java/com/drogbalog/server/domain/user/service/validator/Validator.java
@@ -3,5 +3,5 @@ package com.drogbalog.server.domain.user.service.validator;
 import com.drogbalog.server.domain.user.domain.request.UserRequest;
 
 public interface Validator {
-    boolean signUpValidator(UserRequest request);
+    void execute(UserRequest request);
 }
