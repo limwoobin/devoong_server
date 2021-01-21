194835719666c3cba7119822e1c1edd1f5ffd321
limwoobin <drogba02@naver.com>
2021-01-21 15:22:22 +0900

    Add - logout api 작성

--- src/test/java/com/drogbalog/server/AdminApiTest.java
+++ src/test/java/com/drogbalog/server/AdminApiTest.java
@@ -18,7 +18,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = {TestApi.class})
 public class AdminApiTest {
 
     @Autowired
