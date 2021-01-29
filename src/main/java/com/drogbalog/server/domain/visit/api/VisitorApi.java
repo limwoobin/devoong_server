9af8f8f06cf7178f6d8a884f6bf605a0a31efef6
limwoobin <drogba02@naver.com>
2021-01-29 17:10:27 +0900

    Add - 방문자 조회 API 추가

--- src/main/java/com/drogbalog/server/domain/visit/api/VisitorApi.java
+++ src/main/java/com/drogbalog/server/domain/visit/api/VisitorApi.java
@@ -1,4 +1,27 @@
 package com.drogbalog.server.domain.visit.api;
 
+import com.drogbalog.server.domain.visit.domain.VisitResponse;
+import com.drogbalog.server.domain.visit.service.VisitService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+@Log4j2
+@Api(tags = "Visit Api")
 public class VisitorApi {
+    private final VisitService visitService;
+
+    @GetMapping("")
+    public ResponseEntity<VisitResponse> visit() {
+        VisitResponse visitResponse = visitService.updateVisit();
+        return new ResponseEntity<>(visitResponse , HttpStatus.OK);
+    }
 }
