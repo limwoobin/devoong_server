f1c9be984c04da71282fd2a0131b4b7b3571f282
limwoobin <drogba02@naver.com>
2021-02-21 17:53:18 +0900

    Fix - TestApplication 수정

--- src/test/java/com/drogbalog/server/ServerApplicationTests.java
+++ src/test/java/com/drogbalog/server/ServerApplicationTests.java
@@ -1,14 +1,15 @@
 package com.drogbalog.server;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@Log4j2
+@ActiveProfiles(value = "test")
 class ServerApplicationTests {
 
     @Test
-    void contextLoads() {
-
+    public void test() {
+        log.info("zz");
     }
-
 }
