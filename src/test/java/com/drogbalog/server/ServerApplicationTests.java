a120695adc287a3419336ffd2f008794cf9b6939
limwoobin <drogba02@naver.com>
2021-02-21 18:42:14 +0900

    Fix - ServerApplicationTests profile 수정

--- src/test/java/com/drogbalog/server/ServerApplicationTests.java
+++ src/test/java/com/drogbalog/server/ServerApplicationTests.java
@@ -2,10 +2,12 @@ package com.drogbalog.server;
 
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @Log4j2
-@ActiveProfiles(value = "test")
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(value = "local")
 class ServerApplicationTests {
 
     @Test
