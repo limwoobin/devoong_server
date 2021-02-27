20160261c0c275d868c5eacd5485b2d5e09ba6c5
limwoobin <drogba02@naver.com>
2021-02-27 19:05:06 +0900

    Add - VisitorApiTest 추가

--- src/test/java/com/drogbalog/server/visit/VisitorApiTest.java
+++ src/test/java/com/drogbalog/server/visit/VisitorApiTest.java
@@ -1,7 +1,7 @@
-package com.drogbalog.server.posts;
+package com.drogbalog.server.visit;
 
-import com.drogbalog.server.domain.posts.api.PostsApi;
-import com.drogbalog.server.domain.posts.service.PostsService;
+import com.drogbalog.server.domain.visit.api.VisitorApi;
+import com.drogbalog.server.domain.visit.service.VisitService;
 import com.drogbalog.server.global.config.security.SecurityConfiguration;
 import com.drogbalog.server.global.config.security.jwt.JwtAuthenticationInterceptor;
 import lombok.extern.log4j.Log4j2;
@@ -12,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,16 +19,15 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.core.Is.is;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Log4j2
 @WebMvcTest(
-        controllers = PostsApi.class,
+        controllers = VisitorApi.class,
         excludeFilters = {
                 @ComponentScan.Filter(
                         type = FilterType.ASSIGNABLE_TYPE,
@@ -37,19 +35,21 @@ import static org.hamcrest.core.Is.is;
                 )
         }
 )
-public class PostsApiTest {
+public class VisitorApiTest {
 
     @Autowired
     private WebApplicationContext context;
+
+    @MockBean
+    private VisitService visitService;
 
     @Autowired
     private MockMvc mvc;
 
     @MockBean
-    private PostsService postsService;
-
-    @MockBean
     private JwtAuthenticationInterceptor interceptor;
+
+
 
     @BeforeEach
     public void setUp() {
@@ -61,37 +61,18 @@ public class PostsApiTest {
 
     @Test
     @WithMockUser(roles = "USER")
-    public void basic_test() throws Exception {
-        log.info("Basic Test");
+    public void visitor_test() throws Exception {
+        log.info("Visitor Test");
 
-        final ResultActions actions = mvc.perform(get("/posts/test")
+        final ResultActions actions = mvc.perform(get("/")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print());
 
         actions
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.test" , is("testData")));
-    }
+                .andExpect(jsonPath("$.today" , is(0)))
+                .andExpect(jsonPath("$.allDay" , is(0)));
 
-    @Test
-    public void getSearchAll_test() throws Exception {
-        log.info("PostsApi Controller Test");
-
-        // given
-        final String keyword = "test";
-        final PageRequest pageRequest = PageRequest.of(5 , 5);
-
-        given(postsService.searchAll(keyword , pageRequest));
-
-        // when
-        final ResultActions actions = mvc.perform(get("/posts/search/{keyword}")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print());
-
-        // then
-        actions
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
