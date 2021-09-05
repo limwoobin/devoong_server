12b17e9593f9f615b55624f1c673c77fde0a0e2a
limwoobin <drogba02@naver.com>
2021-09-05 18:10:52 +0900

    modify - test code colifct 수정

--- src/test/java/com/drogbalog/server/domain/user/service/UserServiceTest.java
+++ src/test/java/com/drogbalog/server/domain/user/service/UserServiceTest.java
@@ -91,8 +91,7 @@ class UserServiceTest {
             UserRequest userRequest = 회원가입_신청한_유저;
 
             // when
-            when(passwordEncoder.e
-                    ncode(userRequest.getPassword())).thenReturn("123123123");
+            when(passwordEncoder.encode(userRequest.getPassword())).thenReturn("123123123");
             when(userRepository.save(any())).thenReturn(회원가입된_유저_ENTITY);
 
             // then
